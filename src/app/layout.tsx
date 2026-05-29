79ce999b22ca1743bdbc253d6d0532c2ef8d8cc1
bluewiggle <nfar0019@student.monash.edu>
2026-05-30 08:39:05 +1000

    Add heat map

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,6 +65,16 @@ export default function RootLayout({
           </>
         )}
 
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wywgpzh7ti");
+          `}
+        </Script>
+
         <Script id="bnb-lab-attribution-and-meta-pixel" strategy="afterInteractive">
           {`
             (function () {
